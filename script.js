d3564a0876dd66ee9be19d7d32252419395fd9cb
Mason Scott <scottm7575@gmail.com>
2020-02-07 12:52:07 -0600

    complete javascript for NFL page

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-var queryURL = "https://cors-anywhere.herokuapp.com/https://api.sportradar.us/nfl-t1/teams/hierarchy.json?api_key=b2uf4274y4r7ywx89fmph5e2"
-
-console.log("test")
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response)
-
-=======
 $(document).ready(function() {
 
     $(".move").hover(
@@ -27,5 +15,4 @@ $(document).ready(function() {
             }, 200);
         }
     )
->>>>>>> master
 })
